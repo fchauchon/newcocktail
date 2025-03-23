ac5cf67eb772f75a4401f961687d3eb6927b1f43
Frederic Chauchon <fchauchon@micropole.com>
2025-03-23 23:32:45 +0100

    Fix dataService

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,12 +9,12 @@ export class DataService {
   private readonly cocktails: Array<any> = []
 
   constructor() {
-    this.cocktails.push( { name: 'Mojito', description: 'Rien de mieux qu\'un bon mojito maison fait dans les régles de l\'art', img: '/mojito.jpg', alcohol: true } )
-    this.cocktails.push( { name: 'Margarita', description: 'La Margarita est un cocktail à base de tequila, inventé par des Américains au Mexique', img: '/margarita.jpg', alcohol: true } )
-    this.cocktails.push( { name: 'Sex on the beach', description: 'Le Sex on the Beach est un cocktail alcoolisé contenant de la vodka, du Schnaps à la pêche, du jus d\'orange et du jus de canneberge', img: '/sex-on-the-beach.jpg', alcohol: true } )
-    this.cocktails.push( { name: 'Cuba libre', description: 'Le Cuba libre est un cocktail officiel de l\'IBA4, à base de rhum, citron vert, et cola.', img: '/cuba-libre.jpg', alcohol: true } )
-    this.cocktails.push( { name: 'Apello', description: 'Le Virgin Mojito est inspiré par le célèbre Mojito cubain, l\'un des ceux qui représente le plus la culture cubaine, à l\'égal du Cuba libre et du Daiquiri.', img: '/apello.jpg', alcohol: false } )
-    this.cocktails.push( { name: 'Halloween Punch', description: 'Pour en mettre plein les yeux le soir le plus effrayant de l\'année, préparez un punch d\'Halloween facile… couleur rouge sang et rempli de globes oculaires !', img: '/halloween-punch.jpg', alcohol: false } )
+    this.cocktails.push( { id: 1, name: 'Mojito', description: 'Rien de mieux qu\'un bon mojito maison fait dans les régles de l\'art', img: '/mojito.jpg', alcohol: true } )
+    this.cocktails.push( { id: 2, name: 'Margarita', description: 'La Margarita est un cocktail à base de tequila, inventé par des Américains au Mexique', img: '/margarita.jpg', alcohol: true } )
+    this.cocktails.push( { id: 3, name: 'Sex on the beach', description: 'Le Sex on the Beach est un cocktail alcoolisé contenant de la vodka, du Schnaps à la pêche, du jus d\'orange et du jus de canneberge', img: '/sex-on-the-beach.jpg', alcohol: true } )
+    this.cocktails.push( { id: 4, name: 'Cuba libre', description: 'Le Cuba libre est un cocktail officiel de l\'IBA4, à base de rhum, citron vert, et cola.', img: '/cuba-libre.jpg', alcohol: true } )
+    this.cocktails.push( { id: 5, name: 'Apello', description: 'Le Virgin Mojito est inspiré par le célèbre Mojito cubain, l\'un des ceux qui représente le plus la culture cubaine, à l\'égal du Cuba libre et du Daiquiri.', img: '/apello.jpg', alcohol: false } )
+    this.cocktails.push( { id: 6, name: 'Halloween Punch', description: 'Pour en mettre plein les yeux le soir le plus effrayant de l\'année, préparez un punch d\'Halloween facile… couleur rouge sang et rempli de globes oculaires !', img: '/halloween-punch.jpg', alcohol: false } )
   }
 
   getCocktails(): Observable<any[]> {
