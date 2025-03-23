ff1cbd615caab644528799ee358d8e8234fca526
Frederic Chauchon <fchauchon@micropole.com>
2025-03-23 18:46:48 +0100

    day2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { WithOrWithoutComponent } from './with-or-without/with-or-without.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: '/home' },
+    { path: 'home', component: WithOrWithoutComponent },
+    { path: 'with', component: WithOrWithoutComponent },
+    { path: 'without', component: WithOrWithoutComponent },
+    { path: 'about', component: AboutComponent },
+];
---
+++ src/app/with-or-without/with-or-without.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-with-or-without',
+  imports: [],
+  templateUrl: './with-or-without.component.html',
+  styleUrl: './with-or-without.component.css'
+})
+export class WithOrWithoutComponent {
+
+}
