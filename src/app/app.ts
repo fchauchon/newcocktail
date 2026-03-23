92664c93f12d3f88ac5e97662318c6f10e90a958
Frederic Chauchon <fchauchon@micropole.com>
2026-03-23 12:25:13 +0100

    Fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Footer } from './footer/footer';
-import { Header } from './header/header';
+import { Footer } from "./footer/footer";
+import { Header } from "./header/header";
 import { Intro } from './intro/intro';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Intro, Footer],
-  standalone: true,
+  imports: [RouterOutlet, Header, Footer, Intro],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  title = 'cocktail';
+  protected readonly title = signal('cocktail');
 }
