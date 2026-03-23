68f23548174c51ce31150aac3ad09ad4320dfd23
Frederic Chauchon <fchauchon@micropole.com>
2026-03-23 12:15:03 +0100

    Pfff

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  title = 'cocktail';
+  protected readonly title = signal('cocktail');
 }
